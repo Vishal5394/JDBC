8919d0818c9b29eac3244e2b81c4b756a2fd3cba
Vishal5394 <104050164+Vishal5394@users.noreply.github.com>
2022-08-23 17:48:53 +0530

    Added: retrieve the Employee Payroll from the Database
    Retrieve the Employee Payroll from the Database

--- JdBC.java
+++ JdBC.java
@@ -1,40 +1,32 @@
 package com.BridgeLabz.jdbc;
 
 
+package com.BridgeLabz.jdbc;
+
 import java.sql.*;
-import java.util.Enumeration;
 
-public class JdBC {
-	public static void main(String[] args) {
-		String jdbcURL= "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
-		String userName = "root";
-		String password = "Vishal5394#";
-		Connection con;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("Driver loaded!");
-		} catch (ClassNotFoundException e) {
-			throw new IllegalStateException("Cannot find the driver in te classpath!", e);
-		}
-		
-		listDrivers();
-		
-		try {
-			System.out.println("Connecting to database:"+jdbcURL);
-			con = DriverManager.getConnection(jdbcURL,userName,password);
-			System.out.println("Connection is successful!!!!!"+con);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void listDrivers() {
-		Enumeration<Driver>driverList = DriverManager.getDrivers();
-		while (driverList.hasMoreElements()) {
-			Driver driverClass = (Driver) driverList.nextElement();
-			System.out.println("  "+ driverClass.getClass().getName());
-		}
-		
-	}
-
+public class ReadOperation 
+{
+    public static void main(String[] args) throws SQLException 
+    {
+        String url = "jdbc:mysql://localhost:3306/payroll_service";
+        String user = "root";
+        String password = "Vishal5394#";
+        // get a connection to database
+        Connection connection = DriverManager.getConnection(url, user, password);
+        // create a statement
+        try (Statement statement = connection.createStatement()) 
+        {
+            // execute SQL query
+            String query = "SELECT * FROM employee_payroll;";
+            ResultSet resultSet = statement.executeQuery(query);
+            // process the result set
+            while (resultSet.next()) 
+            {
+                System.out.println(resultSet.getInt(1) + "  " + resultSet.getString(2) + "  " + resultSet.getString(3)+"   "
+                          + resultSet.getString(4)+"  "+ resultSet.getString(5)+"  "+ resultSet.getString(6)+"   "+ resultSet.getString(7)
+                             + "  "+ resultSet.getString(8)+"  "+ resultSet.getString(9)+ "  "+ resultSet.getString(10)+ "  "+ resultSet.getString(11)+ "  "+ resultSet.getString(12));
+            }
+        }
+    }
 }
